ff4c2ce485de0e242a2710e365165168c5d24d31
pablo <pablo0168@hotmail.com>
2017-07-28 22:29:15 -0500

    first files

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import current_app, Flask, redirect, url_for
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import config
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(config) # load config.py
+app.secret_key = 'super duper mega secret key'
+
+login_manager = LoginManager() # Login manager for the application
+login_manager.init_app(app) # apply login manager
+login_manager.login_view = 'home' # set the default redirect page
+
+db = SQLAlchemy(app)
+# This imports are necessary for the scope of the directory structure
+from app import views
+from app import models
+from views import *
